61bcd941a9420be995081501776e8717f89e5535
s-sinha12 <74825648+s-sinha12@users.noreply.github.com>
2020-12-05 20:57:48 +0530

    taking Input and Console

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-//input
-var username = prompt("Give me your username")
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+//console.log(txtInput)
 
-//processing
-var welcomeMessage = "this script works!!" + username
+function clickHandler() {
+    
+    console.log("clicked!");
+    console.log("input", txtInput.value);
+};
 
-//output
-alert(welcomeMessage);
+btnTranslate.addEventListener("click", clickHandler)
+
+
+
